5a925359ff1792acfcbcf125958b8b9d9cadc8bf
Jimmy Ko柯宇倫 <jimmy.ko@gogoro.com>
2024-08-21 19:35:06 +0800

    Add mongodb user

---
+++ src/main/java/com/example/accessingdatamongodb/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.accessingdatamongodb;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    public User findByFirstName(String firstName);
+    public List<User> findByLastName(String lastName);
+}
